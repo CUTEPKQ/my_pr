5feefbff96f1445db639eb660d2b8519d4f4e28f
fyx <3231219729@qq.com>
2023-03-16 18:24:48 +0800

    第二次

--- chnage.py
+++ chnage.py
@@ -2,7 +2,7 @@
 Author: fyx
 Date: 2023-03-16 18:13:57
 LastEditors: [you name]
-LastEditTime: 2023-03-16 18:13:58
+LastEditTime: 2023-03-16 18:24:14
 Description: 
 '''
-print("hello")
+print("hello 我好帅")
